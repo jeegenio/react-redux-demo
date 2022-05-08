3917622c1df178e940fd212939b4997eea49a213
Jee Ryann Genio <jeegenio@gmail.com>
2022-05-08 15:30:48 +0800

    initial commit

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function CakeContainer() {
+  return (
+    <div>
+      <h2>Number of Cakes</h2>
+      <button>Buy Cake</button>
+    </div>
+  );
+}
+
+const mapStateToProps = state => {
+  return{
+    numberOfCakes: state.numberOfCakes
+  }
+}
+export default CakeContainer;
